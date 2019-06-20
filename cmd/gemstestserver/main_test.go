c79768287394da53a7989acba8491f90236411ee
Leo Zhang <leo@fossa.io>
2019-06-20 15:42:48 -0700

    test: Update test file path

--- cmd/gemstestserver/main_test.go
+++ cmd/gemstestserver/main_test.go
@@ -9,23 +9,19 @@ import (
 	"github.com/fossas/gemstest/bindata"
 )
 
-type Versions struct {
-	Versions [][]string
-}
-
 func TestVersionsAreValidSemver(t *testing.T) {
-	fixtures, err := bindata.Asset("../../bindata/versions.json")
+	fixtures, err := bindata.Asset("../../bindata/data/versions.json")
 	if err != nil {
 		t.Error(err)
 	}
 
-	var versions Versions
+	var versions [][]string
 	err = json.Unmarshal(fixtures, &versions)
 	if err != nil {
 		t.Error(err)
 	}
 
-	for _, pairs := range versions.Versions {
+	for _, pairs := range versions {
 		pkg := pairs[0]
 		version := pairs[1]
 		_, err := semver.NewVersion(version)
